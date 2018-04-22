56d6d42776da9fb317bac9dd65bf65bbbeca882f
Malcolm Flint <malcolm.flint@gmail.com>
2018-04-22 15:09:50 -0700

    It works!

--- multiLineChart.js
+++ multiLineChart.js
@@ -4,9 +4,21 @@ d3.text("EPC_2000_2010.csv", function (t) {
         let data = Array(rows[0].length - 1).fill({});
         for (let row = 0; row < rows.length; row++) {
             for (let column = 1; column < rows[0].length; column++) {
-                data[column - 1][rows[row][0]] = rows[row][column];
+                let new_data = {};
+                new_data[rows[row][0]] = rows[row][column];
+                data[column - 1] = Object.assign(new_data, data[column - 1]);
+                console.log(data[column - 1]);
             }
         }
         console.log(data);
-        return data;
     });
+//
+// d3.csv("EPC_2000_2010", function (d) {
+//     let transposed = [];
+//     d.forEach(function (d) {
+//         for (let key in d) {
+//
+//         }
+//
+//     })
+// })
